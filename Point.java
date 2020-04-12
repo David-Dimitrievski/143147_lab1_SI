ecb03271808b612fc31fcc73fe3a7f5f7963c858
David Dimitrevski <david@live.com>
2020-04-12 22:55:34 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -3,8 +3,46 @@ class Point {
 	double x,y;
     String color
 
+	public String getId() {
+		return id;
+	}
 
-	//TODO constructor
+	public double getX() {
+		return x;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public Point(String id, double x, double y, String color) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+
+//TODO constructor
 
 	//TODO setters and getters
 
